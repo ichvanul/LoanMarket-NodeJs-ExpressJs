041e435653ef4cb29749a3edfdc38499f63a5f6a
ichvanul <ichvanulyp@gmail.com>
2020-06-23 11:00:43 +0700

    detail page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require ('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const port = 5000;
 const bodyParser = require('body-parser');
 const route = require('./src/routers/index.js');
@@ -11,6 +12,18 @@ app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+//set views file
+app.engine('handlebars', exphbs ({ defaultLayout: 'main' }));
+//set view engine
+app.set('view engine', 'handlebars');
+
+app.get('/branch', (req, res) => {
+  res.render('index')
+})
+
+app.get('/adviser', (req, res) => {
+  res.render('adviser')
+})
 
 app.use('/api/v1/', route);
 app.listen(port, ()=>{
